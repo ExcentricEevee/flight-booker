5d247dba995091dbdd1de8a83e0f333a49151a89
Curtis <excentriceevee@hotmail.com>
2022-07-13 05:13:05 -0400

    Add partials for forms to organize cod better. Add conditional to index.html so the flight_form only renders if a search has been made, and won't crash the site if there hasn't been one yet.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,8 +3,10 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
 
   def self.search_query(params)
+    #retreive a list of flights based on selected to and from airports
     flights = Flight.where(departure_airport_id: params[:departing_airport]).where(arrival_airport_id: params[:arriving_airport])
 
+    #now filter that list based on when they want to go
     formatted_search_date = Flight.formatted_date(params[:date])
     result = flights.select { |flight| flight.start_time.to_date == formatted_search_date }
   end
@@ -14,16 +16,5 @@ class Flight < ApplicationRecord
   def self.formatted_date(date_hash)
     formatted_date = "#{date_hash[:month]}/#{date_hash[:day]}/#{date_hash[:year]}"
     parsed_date = Date.strptime(formatted_date, "%m/%d/%Y")
-  end
-
-  def self.flights_with_matching_dates(search_date)
-    result = Array.new
-    Flight.find_each do |flight|
-      if flight.flight_date_formatted == search_date
-        result << flight
-      end
-    end
-
-    result
   end
 end
