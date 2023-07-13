b1d3d0614d1ae18871eae15252d2d212a3d8c0b3
Curtis <excentriceevee@hotmail.com>
2023-07-12 22:16:28 -0400

    Creat Flight model/table with its own attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_010754) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_021027) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_010754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["code"], name: "index_airports_on_code"
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.datetime "start_time"
+    t.time "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
