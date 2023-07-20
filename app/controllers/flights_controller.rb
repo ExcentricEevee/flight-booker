1c796d80a0194ae33c9434eae695cbc1b18b2b44
Curtis <excentriceevee@hotmail.com>
2023-07-19 23:08:26 -0400

    Clean up to put flight search logic to the model instead of on the view

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,13 @@ class FlightsController < ApplicationController
         @airport_options = Airport.all.order(:code).map{ |a| [ a.code, a.id ] }
         # list only the dates that appear in flights
         @date_options = Flight.all.order(:start_time)
-                            .map{ |f| [ f.start_time_formatted, f.start_time ] }
+                            .map{ |f| [ f.start_time_formatted, f.start_time_formatted ] }
+        @flights = Flight.flight_search(search_params)
+    end
+
+    private
+
+    def search_params
+        params.permit(:departure_airport_id, :arrival_airport_id, :start_time)
     end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,11 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
+  def self.flight_search(params)
+    Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND DATE(start_time) = ?",
+          params[:departure_airport_id], params[:arrival_airport_id], params[:start_time])
+  end
+
   def start_time_formatted
     start_time.strftime("%m/%d/%Y")
   end
