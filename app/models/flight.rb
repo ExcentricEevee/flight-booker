e3612da2fe7c87d3ad6f367fe9bf702212c063ab
Curtis <excentriceevee@hotmail.com>
2023-07-19 23:26:44 -0400

    Make flight_search's query more readable

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,8 +3,9 @@ class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: "Airport"
 
   def self.flight_search(params)
-    Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND DATE(start_time) = ?",
-          params[:departure_airport_id], params[:arrival_airport_id], params[:start_time])
+    Flight.where("departure_airport_id = ?", params[:departure_airport_id])
+          .where("arrival_airport_id = ?", params[:arrival_airport_id])
+          .where("DATE(start_time) = ?", params[:start_time])
   end
 
   def start_time_formatted
