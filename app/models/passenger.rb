a39800baf22868a348edd6f32836f8135472715e
Curtis <excentriceevee@hotmail.com>
2023-09-23 00:57:50 -0400

    Create basic essentials for a confirmation_email action

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,6 +1,5 @@
 class Passenger < ApplicationRecord
     belongs_to :booking, class_name: "Booking"
-    # has_many :flights, through: :bookings
 
     validates :name, presence: true, length: { in: 2..36 }
     validates :email, presence: true
